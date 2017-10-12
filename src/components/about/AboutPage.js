37885548670885f23b5e42b5495381f7c7ea07e4
Raymond Yu <ryu2@us.costar.local>
2017-10-12 15:53:48 -0700

    skeleton working site

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1> About </h1>
+                <p> This app uses React, Redux, and React Router and other tools.</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello i am writing react.js code...');
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+import './style/style.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+    <Router history = {browserHistory} routes ={routes} />, document.getElementById('app')
+);
